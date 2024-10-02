87c620c3b9d0da4df17d8bbf1685c3b2cc62a684
Nickolay Petrushin <nick.mflj@gmail.com>
2024-10-02 23:15:10 +0300

    for verification

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        try (Connection connection = Util.getConnection()) {
+            UserServiceImpl service = new UserServiceImpl();
+            service.createUsersTable();
+            service.saveUser("Вася", "Пупкин",  Byte.valueOf("7"));
+            service.saveUser("тетя", "Мотя",  Byte.valueOf("64"));
+            service.saveUser("дядя", "Вася",  Byte.valueOf("57"));
+            service.saveUser("бабка", "На скамейке",  Byte.valueOf("81"));
+            service.getAllUsers().stream().forEach(System.out::println);
+            service.cleanUsersTable();
+            service.dropUsersTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String loginDB = "sa";
+    private static final String passwordDB = "Admin";
+    private static final String urlDB = "jdbc:mysql://localhost:3306/kata?serverTimezone=UTC";
+    private static Connection connection;
+
+    private static Connection getConnectionBD() {
+        try  {
+            connection = DriverManager.getConnection(urlDB, loginDB, passwordDB);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static Connection getConnection() {
+        return connection == null ? getConnectionBD() : connection;
+    }
 }
